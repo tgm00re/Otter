9a059b321ffdf4d1500d5340fb239f565360f1e7
Thomas Moore <tmoorecode@gmail.com>
2022-04-21 16:35:47 -0400

    initial commit, project setup including react, some dependencies, spring web, sql config

---
+++ src/main/java/com/thomasmoore/otter/OtterApplication.java
@@ -0,0 +1,13 @@
+package com.thomasmoore.otter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomasmoore/otter/OtterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasmoore.otter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
