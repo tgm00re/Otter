1a10c74d9a5b4af0f6563f38241b107a1f894625
Thomas Moore <tmoorecode@gmail.com>
2022-04-22 15:23:17 -0400

    added rest controller for users and posts

--- src/main/java/com/thomasmoore/otter/mvc/controllers/PostController.java
+++ src/main/java/com/thomasmoore/otter/mvc/controllers/PostController.java
@@ -1,5 +1,52 @@
 package com.thomasmoore.otter.mvc.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thomasmoore.otter.mvc.models.Post;
+import com.thomasmoore.otter.mvc.services.PostService;
+
+@RestController
 public class PostController {
 
+	@Autowired
+	private PostService postServ;
+	
+	//================== Create ==================
+	@PostMapping("/api/posts/create")
+	public Post createPost(@RequestBody Post post) {
+		return postServ.create(post);
+	}
+	
+	//================== Read ==================
+	@GetMapping("/api/posts")
+	public List<Post> allPosts(){
+		return postServ.findAll();
+	}
+	
+	@GetMapping("/api/posts/{id}")
+	public Post postById(@PathVariable("id") Long id) {
+		return postServ.findOneById(id);
+	}
+	
+	//================== Update ==================
+	@PutMapping("/api/posts/update")
+	public Post updatePost(@RequestBody Post post) {
+		return postServ.update(post);
+	}
+	
+	
+	//================== Delete ==================
+	@DeleteMapping("/api/posts/delete/{id}")
+	public void deletePost(@PathVariable("id") Long id) {
+		postServ.deleteById(id);
+	}
 }
--- src/main/java/com/thomasmoore/otter/mvc/controllers/UserController.java
+++ src/main/java/com/thomasmoore/otter/mvc/controllers/UserController.java
@@ -1,5 +1,52 @@
 package com.thomasmoore.otter.mvc.controllers;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thomasmoore.otter.mvc.models.User;
+import com.thomasmoore.otter.mvc.services.UserService;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserService userServ;
+	
+	//================== Create ==================
+	
+	@PostMapping("/api/users/create")
+	public User createUser(@RequestBody User user) {
+		return userServ.create(user);
+	}
+	
+	//================== Read ==================
+	@GetMapping("/api/users")
+	public List<User> allUsers(){
+		return userServ.findAll();
+	}
+	
+	@GetMapping("/api/users/{id}")
+	public User userById(@PathVariable("id") Long id) {
+		return userServ.findOneById(id);
+	}
+	
+	//================== Update ==================
+	@PutMapping("/api/users/update")
+	public User updateUser(@RequestBody User user) {
+		return userServ.update(user);
+	}
+	
+	//================== Delete ==================
+	@DeleteMapping("/api/users/delete/{id}")
+	public void deleteUser(@PathVariable("id") Long id) {
+		userServ.delete(id);
+	}
 }
